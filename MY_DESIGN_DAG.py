040a6875d1b5fa5b07c96e9f9768d9cf858c38f7
zhangshijia <3299688465@qq.com>
2023-05-28 21:16:30 +0800

    张世佳 利用QT生成DAG基本界面

---
+++ MY_DESIGN_DAG.py
@@ -0,0 +1,43 @@
+import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QMainWindow, QApplication
+
+from DAG_UI import Ui_MainWindow_DAG
+from PyQt5.QtWidgets import QFileDialog, QMainWindow
+
+
+class MyDesiger_DAG(Ui_MainWindow_DAG, QMainWindow):
+    def __init__(self, parent=None):
+        super(MyDesiger_DAG, self).__init__(parent)
+        self.setWindowTitle("DAG优化")
+        self.setupUi(self)
+        # 设置响应槽（信号源 conect 槽）
+        self.pushButton.clicked.connect(self.open_text)
+
+    def check_charset(self, file_path):
+        import chardet
+        with open(file_path, "rb") as f:
+            data = f.read(1000)
+            charset = chardet.detect(data)['encoding']
+        return charset
+
+    def open_text(self):
+        # 定义打开文件夹目录的函数
+        try:
+            # fname = QFileDialog.getOpenFileName(None, 'Open file')
+            fname = QFileDialog.getOpenFileName(self, 'Open file')
+            if fname[0]:
+                print(fname[0])
+                with open(fname[0], encoding=self.check_charset(fname[0])) as f:
+                    str = f.read()
+                    print(str)
+                    self.textEdit.setText(str)
+        except Exception as e:
+            print("Error: ", e)
+
+
+app = QApplication(sys.argv)
+LL_window = MyDesiger_DAG()
+LL_window.show()
+sys.exit(app.exec_())
