b8d056d909479e66af49e94ea7182fd8677e5cae
zhangshijia <3299688465@qq.com>
2023-05-30 09:52:27 +0800

    张世佳 DAG界面完善

--- MY_DESIGN_DAG.py
+++ MY_DESIGN_DAG.py
@@ -4,9 +4,8 @@ from PyQt5.QtWidgets import QMainWindow, QApplication, QMessageBox
 
 from DAG_UI import Ui_MainWindow_DAG
 from PyQt5.QtWidgets import QFileDialog, QMainWindow
-from create_DAG import create_DAG, optimize, DAG_draw # DAG模型
+from create_DAG import create_DAG, optimize, DAG_draw  # DAG模型
 from PyQt5 import QtGui, QtCore, QtWidgets
-
 
 
 class MyDesiger_DAG(Ui_MainWindow_DAG, QMainWindow):
@@ -43,7 +42,6 @@ class MyDesiger_DAG(Ui_MainWindow_DAG, QMainWindow):
 
         self.textEdit_2.show()
 
-
     def check_charset(self, file_path):
         import chardet
         with open(file_path, "rb") as f:
@@ -64,7 +62,6 @@ class MyDesiger_DAG(Ui_MainWindow_DAG, QMainWindow):
                     self.textEdit.setText(str)
         except Exception as e:
             print("Error: ", e)
-
 
 
 app = QApplication(sys.argv)
