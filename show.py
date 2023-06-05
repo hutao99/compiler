3ab32ca6d9157d6a3c3483d1fbdd750b5bdd0fa5
zhangshijia <3299688465@qq.com>
2023-06-05 15:28:49 +0800

    张世佳 界面功能完善，添加图片查看，导出等功能

--- show.py
+++ show.py
@@ -330,7 +330,6 @@ class Ui_MainWindow(object):
         QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
 
-
     def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
         MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
