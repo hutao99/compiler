ace85bf48cb4eac77a75916bc360e455dba25f36
zhangshijia <3299688465@qq.com>
2023-06-05 13:47:10 +0800

    张世佳 DAG界面功能完善，添加图片查看，导出等功能

--- MY_DESIGN_DAG.py
+++ MY_DESIGN_DAG.py
@@ -89,6 +89,7 @@ class MyDesiger_DAG(Ui_MainWindow_DAG, QMainWindow):
         dialog.setModal(True)
         dialog.exec_()
 
+
 # app = QApplication(sys.argv)
 # LL_window = MyDesiger_DAG()
 # LL_window.show()
