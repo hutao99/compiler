5e320759c8551547f7f4b274a2b84c5512d42ae7
zhangshijia <3299688465@qq.com>
2023-06-05 15:34:28 +0800

    张世佳 DAG界面完善

--- DAG_UI.py
+++ DAG_UI.py
@@ -23,6 +23,7 @@ class Ui_MainWindow_DAG(object):
         # 设置提示文本
         self.textEdit.setPlaceholderText("存放待分析的四元式列表")
 
+
         font = QtGui.QFont()
         font.setFamily("仿宋")
         font.setPointSize(15)
