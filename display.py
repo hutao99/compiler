d03c3eea3708b0628bbf84f21f342a6da28a2cf6
欢 <2116777849@qq.com>
2023-06-05 23:29:48 +0800

    陈瑞:调整输出顺序

--- display.py
+++ display.py
@@ -549,7 +549,7 @@ class DetailUI(Ui_MainWindow, QMainWindow):
                         self.fun_list, self.function_param_list, self.function_jubu_list, self.siyuanshi, self.yufa_Rrror, self.worrings_str, self.text1, self.text2 = rda.solve(
                             self.lbword)
                         text1 = "语法错误处理：\n" + self.yufa_Rrror + "语义错误：\n" + self.worrings_str
-                        all_text = self.text1 + '\n' + self.text2 + '\n' + text1
+                        all_text = text1 + self.text1 + '\n' + self.text2 + '\n'
                         self.textEdit_2.setText(all_text)
                         # 设置图片路径
                         self.textEdit_3.clear()
