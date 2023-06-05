cd3f4de153fe480a126ce9ee789f0accdb7bba11
zhangshijia <3299688465@qq.com>
2023-06-05 13:57:17 +0800

    张世佳 DAG界面功能完善，添加图片查看，导出等功能

--- MY_DESIGN_DAG.py
+++ MY_DESIGN_DAG.py
@@ -27,6 +27,7 @@ class MyDialog(QDialog):
         event.accept()
 
 
+
 class MyDesiger_DAG(Ui_MainWindow_DAG, QMainWindow):
     def __init__(self, parent=None):
         super(MyDesiger_DAG, self).__init__(parent)
